5f312b221c4dac6f799f635dc39c3e8bd2780217
Tako Desktop <takotabak@gmail.com>
2020-07-21 11:20:19 +0200

    fix sysntax w :monkey_face:

--- lib/main.dart
+++ lib/main.dart
@@ -74,27 +74,34 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   void _pushSaved() {
-    Navigator.of(context)
-        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
-      final tiles = _saved.map((WordPair pair) {
-        return ListTile(
-            title: Text(
-          pair.asPascalCase,
-          style: _biggerFont,
-        ));
-      });
-      final divided = ListTile.divideTiles(
-        context = context,
-        tiles: tiles,
-      ).toList();
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
 
-      return Scaffold(
-          appBar: AppBar(
-            title: Text('Saved Suggestions'),
-            actions: [],
-          ),
-          body: ListView(children: divided));
-    }));
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+              actions: [],
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
   }
 }
 
