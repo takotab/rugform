c8d0bf9aa0e3def632938514a14f38920e730cf8
Tako CarbonX <takotabak@gmail.com>
2020-07-22 15:12:06 +0200

    counter included

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class TutorialHome extends StatelessWidget {
       // body is the majority of the screen.
       body: Center(
         child: Column(children: <Widget>[
+          Counter(),
           Expanded(child: MyButton()),
           Text('Hello, world!')
         ]),
@@ -63,5 +64,53 @@ class MyButton extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({this.count});
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: onPressed,
+      child: Text('Increment'),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      ++_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[
+      CounterIncrementor(onPressed: _increment),
+      CounterDisplay(count: _counter),
+    ]);
   }
 }
